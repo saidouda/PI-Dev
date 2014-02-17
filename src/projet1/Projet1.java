442fb9552fb77fc82db711fe6f14ec03e9747753
win8 <win8@192.168.1.8>
2014-02-17 11:59:01 +0100

    commit par saidouda

--- src/projet1/Projet1.java
+++ src/projet1/Projet1.java
@@ -16,6 +16,7 @@ public class Projet1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("bonjour");
         // TODO code application logic here
     }
     
